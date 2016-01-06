d1f13a5de55777ddf6a6bd575408c35da9c7a378
Andrew Khmylov <andrew.khmylov@gmail.com>
2016-01-06 13:39:17 +0300

    Update to version 0.1.1

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,4 @@
-SET Version=0.1.0
+SET Version=0.1.1
 
 nuget\nuget.exe pack src\Tp.I18n.Interfaces\Tp.I18n.Interfaces.csproj -Build -Symbols -Properties Configuration=Release -Version %VERSION%
 nuget\nuget.exe push Tp.I18n.Interfaces.%VERSION%.nupkg
